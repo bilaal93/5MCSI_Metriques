ded97e9163b6690f32ff15a39a6110e1beca2c13
bilaal93 <103037782+bilaal93@users.noreply.github.com>
2025-12-03 10:16:24 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,15 +5,20 @@ from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
-app = Flask(__name__)                                                                                                                  
+app = Flask(__name__)      
+@app.route("/contact/")
+def MaPremiereAPI():
+    return "<h2>Ma page de contact</h2>"
                                                                                                                                        
 @app.route('/')
 def hello_world():
     return render_template('hello.html') #Comm2
-@app.route("/contact/")
-def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
-  @app.route('/tawarano/')
+  
+if __name__ == "__main__":
+  app.run(debug=True)
+
+
+@app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
     raw_content = response.read()
@@ -28,5 +33,3 @@ def meteo():
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
-if __name__ == "__main__":
-  app.run(debug=True)
